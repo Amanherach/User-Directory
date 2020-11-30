b08f8ab7b4baebf0ba5de9b423a3648db78da1e0
Aman Chaudhry <aman.chaudhry@hotmail.com>
2020-11-29 22:10:17 -0700

    index.js, header, nav, wrapper, dataTable

---
+++ public/src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchBox from "../SearchBox";
+
+
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+       <div className="search-area col-4">
+        <SearchBox />
+    </div>
+</nav>
+    );
+}
+export default Nav;
